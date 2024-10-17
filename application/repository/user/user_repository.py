4751baffd1e44ec5e731722bc98ff921f01b2e20
LucasBissonho <lucasbissonho@protonmail.com>
2024-10-17 00:29:48 +0000

    feat: add UserRepository abs class

---
+++ application/repository/user/user_repository.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+from ....domain.user import User
+
+
+class UserRepository(ABC):
+    @abstractmethod
+    async def create_user(self, u: User):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_user_by_username(self, username: str) -> User:
+        raise NotImplementedError
+
+    # @abstractmethod
+    # async def get_user_by_email(self, email: str) -> User:
+    #     raise NotImplementedError
+
+    @abstractmethod
+    async def get_all_users(self) -> list[User]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update_user(self, u: User):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def delete_user(self, u: User):
+        raise NotImplementedError
